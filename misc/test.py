c9c2a4b879ec629d5ac82e57ddc204c9cf11f398
barryallen16 <rjayadithya16@gmail.com>
2026-01-07 16:10:10 +0530

    kept only api code

--- misc/test.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from docling.document_converter import DocumentConverter
-source = Path("timetable.jpeg")
-converter = DocumentConverter()
-result=converter.convert(source=source)
-print(result.document.export_to_markdown())
