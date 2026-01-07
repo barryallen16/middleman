1352887655d3c6a3f7f84a159afcebd4314b623a
barryallen16 <rjayadithya16@gmail.com>
2026-01-07 14:40:54 +0530

    before restructuring

--- api/app.py
+++ api/app.py
@@ -1,6 +1,26 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import shutil
 from fastapi.middleware.cors import CORSMiddleware
+from google import genai
+from google.genai import types
+import os 
+from dotenv import load_dotenv
+import pandas as pd 
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# files used 
+# 1.grade-points.jsonl
+# 2. merged_credits.jsonl
+
+
+load_dotenv()
+GEMINI_API_KEY=os.getenv('RESULTS_PROJ_APIKEY')
+
+client=genai.Client(api_key=GEMINI_API_KEY)
 
 app = FastAPI()
 
@@ -12,18 +32,128 @@ app.add_middleware(
     allow_credentials=True
 )
 
+grade_points_map = {}
+subject_metadata = {}
+
+def load_static_data():
+    try:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        static_dir = os.path.jsoin(base_path, "../static")
+        with open(os.path.join(static_dir, "grade-points.jsonl"), 'r', encoding="utf-8") as in_file:
+            for line in in_file:
+                data=json.loads(line)
+                grade_points_map[data['letter_grade']]=data['grade_points']
+        with open(os.path.join(static_dir,"merged_credits.jsonl"), 'r', encoding="utf-8") as in_file:
+            for line in in_file:
+                data=json.loads(line)
+                subject_metadata[data['subject_code']] = {'credits':data['credits'], 'name': data['subject_name']}
+        logger.info("static data loaded successfully")
+    except Exception as err:
+        logger.error("error while loading static data: ", err)
+    
+load_static_data()
+
+def clean_llm_json_response(text: str) -> str:
+    text = text.strip()
+    if text.startswith("```"):
+        first_newline = text.find("\n")
+        if first_newline != -1:
+            text = text[first_newline + 1 :]
+        if text.endswith("```"):
+            text= text[:-3]        
+    return text.strip()
+
+def calculate_gpa_logic(jsonl_string:str):
+    cleaned_text=clean_llm_json_response(jsonl_string)
+    lines = [line for line in cleaned_text.split("\n") if line.strip()]
+
+    final_list=[]
+
+    for line in lines:
+            try:     
+                line = json.loads(line)
+                current_dict={}
+                acq_credits=0
+                total_credits=0
+                all_data= line
+                results_data = all_data["results"]
+                current_dict["student_regno"] = line["student_regno"]
+                current_dict["student_name"] = line["student_name"]
+                current_dict["results"] = []
+                for data in results_data:     
+                        results_dict={}
+                        results_dict["subject_code"] = data['subject_code']
+                        result= df.loc[df['subject_code'] == data['subject_code'], 'subject_name']
+                        results_dict["subject_name"] = result.values[0]
+                        results_dict["grade"] = data['grade']
+                        acq_credits+=grade_points[data['grade']] * credits[data['subject_code']]
+                        total_credits+=credits[data['subject_code']]
+                        current_dict["results"].append(results_dict)
+            except KeyError as err:
+                print('error:', err)
+            gpa = acq_credits / total_credits
+            gpa = round(gpa, 2)
+            current_dict["gpa"]= gpa
+            final_list.append(current_dict)
+    # print(current_dict)
+    return final_list
+
+def doOCr(image_bytes : bytes):
+
+    prompt ="""
+    **System Role:**
+    You are a specialized OCR extraction engine designed to process academic result sheets. Your output must be strictly valid machine-readable code.
+
+    **Task:**
+    Extract student registration details and examination results from the provided image.
+
+    **Output Format Rules:**
+    1.  **Format:** Return the data in **JSONL (JSON Lines)** format.
+    2.  **Structure:** Each line must represent a **single student** and contain all their subject results.
+    3.  **No Markdown:** Do not use markdown blocks (like ```json). Just return the raw text lines.
+    4.  **Schema:** Follow this exact JSON structure for every line:
+        {"student_regno": "STRING", "student_name": "STRING", "results": [{"subject_code": "STRING", "grade": "STRING"}, {"subject_code": "STRING", "grade": "STRING"}]}
+
+    **Extraction Rules:**
+    1.  **Distinguish Characters:** Be extremely careful with 'O' (letter) versus '0' (zero).
+    2.  **Index 6 Correction:**  If you detect the number '1' at index 6 of any `subject_code, you must correct it to 'I'.
+    3.  **Multiple Students:** If the image lists multiple students, generate one JSON line per student.
+    4.  **Error Handling:** If the text is too blurry, cropped, or illegible to extract data with high confidence, return exactly this JSON object on a single line:
+        {"error": "IMAGE_UNCLEAR", "message": "Please upload a clearer image."}
+    """
+    try:
+            
+        response = client.models.generate_content(model="gemini-2.5-flash", 
+                                                contents=[
+                                                    types.Part.from_bytes(data=image_bytes,
+                                                                            mime_type="image/jpeg")
+                                                                            ,prompt])
+        return response.text
+    except Exception as e:
+        logger.error(f"Gemini API error : {e}")
+        raise HTTPException(status_code=500, detail= "Error communicating with Gemini API service")
+
 @app.get("/")
 def root():
-    return {"hello":"world"}
+    return {"status":"active", "message": "MGR GPA calculator API"}
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    with open(f"saved_{file.filename}", "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-        
-        return {"filename": file.filename,
-                "saved":f"saved_{file.filename}"}
-    
 @app.get("/test/")
 def returning_test():
     return {'student_regno': '221191101064', 'student_name': 'JAYADITHYA R', 'results': [{'subject_code': 'EBCC22I07', 'subject_name': 'SOFT SKILL II -QUALITATIVE AND QUANTITATIVE SKILLS', 'grade': 'S'}, {'subject_code': 'EBCS22009', 'subject_name': 'OBJECT ORIENTED SOFTWARE ENGINEERING', 'grade': 'A'}, {'subject_code': 'EBCS22010', 'subject_name': 'WEB DESIGN USING PHP & MYSQL', 'grade': 'A'}, {'subject_code': 'EBCS22E11', 'subject_name': 'CRYPTOGRAPHY AND NETWORK SECURITY', 'grade': 'A'}, {'subject_code': 'EBCS22L07', 'subject_name': 'OBJECT ORIENTED SOFTWARE ENGINEERING LAB', 'grade': 'H'}, {'subject_code': 'EBCS22L08', 'subject_name': 'WEB DESIGN USING PHP& MYSQL LAB', 'grade': 'H'}, {'subject_code': 'EBDS22ET6', 'subject_name': 'GENERATIVE AI', 'grade': 'S'}, {'subject_code': 'EBDS22I03', 'subject_name': 'TECHNICAL SKILL III', 'grade': 'H'}, {'subject_code': 'EBDS22I04', 'subject_name': 'MINI PROJECT/INTERNSHIP', 'grade': 'H'}, {'subject_code': 'EBEC22OE2', 'subject_name': 'Cellular Mobile communication', 'grade': 'S'}], 'gpa': 8.75}
+
+@app.post("/calculateGpa/")
+async def gpa_calculation(file: UploadFile = File(...)):
+    saved_filename=f"saved_{file.filename}"
+    with open(saved_filename, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return calculateGpa(doOCr(saved_filename))
+
+    
+@app.get("/return/")
+def return_jsonlist():
+    string_ ="""{"student_regno": "221191101013", "student_name": "ALHAMEEM S", "results": [{"subject_code": "EBCC22I07", "grade": "B"}, {"subject_code": "EBCS22009", "grade": "C"}, {"subject_code": "EBCS22010", "grade": "C"}, {"subject_code": "EBCS22E11", "grade": "F"}, {"subject_code": "EBCS22L07", "grade": "S"}, {"subject_code": "EBCS22L08", "grade": "S"}, {"subject_code": "EBDS22ET6", "grade": "B"}, {"subject_code": "EBDS22I03", "grade": "B"}, {"subject_code": "EBDS22I04", "grade": "B"}, {"subject_code": "EBEE22OE6", "grade": "A"}, {"subject_code": "EBCS22006", "grade": "B"}, {"subject_code": "EBCS22007", "grade": "C"}]}
+{"student_regno": "221191101047", "student_name": "ESHAA", "results": [{"subject_code": "EBCC22I07", "grade": "A"}, {"subject_code": "EBCS22009", "grade": "C"}, {"subject_code": "EBCS22010", "grade": "C"}, {"subject_code": "EBCS22E11", "grade": "F"}, {"subject_code": "EBCS22L07", "grade": "S"}, {"subject_code": "EBCS22L08", "grade": "S"}, {"subject_code": "EBDS22ET6", "grade": "B"}, {"subject_code": "EBDS22I03", "grade": "A"}, {"subject_code": "EBDS22I04", "grade": "S"}, {"subject_code": "EBEE22OE8", "grade": "B"}, {"subject_code": "EBBT22OE1", "grade": "A"}]}
+{"student_regno": "221191101064", "student_name": "JAYADITHYA R", "results": [{"subject_code": "EBCC22I07", "grade": "S"}, {"subject_code": "EBCS22009", "grade": "A"}, {"subject_code": "EBCS22010", "grade": "A"}, {"subject_code": "EBCS22E11", "grade": "A"}, {"subject_code": "EBCS22L07", "grade": "H"}, {"subject_code": "EBCS22L08", "grade": "H"}, {"subject_code": "EBDS22ET6", "grade": "S"}, {"subject_code": "EBDS22I03", "grade": "H"}, {"subject_code": "EBDS22I04", "grade": "H"}, {"subject_code": "EBEC22OE2", "grade": "S"}]}
+{"student_regno": "221191101088", "student_name": "MD TAQIYY FAIZ M", "results": [{"subject_code": "EBCC22I07", "grade": "A"}, {"subject_code": "EBCS22009", "grade": "A"}, {"subject_code": "EBCS22010", "grade": "A"}, {"subject_code": "EBCS22E11", "grade": "B"}, {"subject_code": "EBCS22L07", "grade": "H"}, {"subject_code": "EBCS22L08", "grade": "S"}, {"subject_code": "EBDS22ET6", "grade": "S"}, {"subject_code": "EBDS22I03", "grade": "S"}, {"subject_code": "EBDS22I04", "grade": "S"}, {"subject_code": "EBME22OE7", "grade": "S"}, {"subject_code": "EBCS22006", "grade": "B"}, {"subject_code": "EBCT22OE5", "grade": "A"}]}
+{"student_regno": "221191101129", "student_name": "SASI KUMAR M", "results": [{"subject_code": "EBCC22I07", "grade": "A"}, {"subject_code": "EBCS22009", "grade": "A"}, {"subject_code": "EBCS22010", "grade": "A"}, {"subject_code": "EBCS22E11", "grade": "B"}, {"subject_code": "EBCS22L07", "grade": "H"}, {"subject_code": "EBCS22L08", "grade": "H"}, {"subject_code": "EBDS22ET6", "grade": "S"}, {"subject_code": "EBDS22I03", "grade": "H"}, {"subject_code": "EBDS22I04", "grade": "S"}, {"subject_code": "EBEE22OE6", "grade": "S"}]}"""
+    return calculateGpa(string_)
--- wapp/test.js
+++ wapp/test.js
@@ -1,38 +1,260 @@
 const studentName = document.getElementById("stdname");
 const studentRegno = document.getElementById("stdregno");
 const resultDisplay = document.getElementById("resultDisplay");
-async function testing_response() {
-  response = await fetch("http://localhost:8000/test/", {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      studentRegno.textContent = data.student_regno;
-      studentName.textContent = data.student_name;
-      resultDisplay.innerHTML += `<li class="w-[90%] bg-white rounded-lg flex px-4 py-2 justify-between gap-4 text-black border-3">
-            <h2>Your GPA is:</h2>
-            <h1 class=" text-4xl font-semibold">
-              ${data.gpa}
-            </h1>
-      </li>`;
-      data.results.forEach((result) => {
-        resultDisplay.innerHTML += `<li
-          class="w-[90%] bg-blue-700 rounded-lg flex px-4 py-2 justify-between gap-4"
-        >
-          <div><h2>${result.subject_code}</h2>
-          <h1 class="font-semibold">
-            ${result.subject_name}
-            </h1>
-          </div>
-          <div class="text-center">
-          <h1 class="text-4xl font-bold">${result.grade}</h1>
-            <h2>Grade</h2>
-          </div>
-        </li>
-        `;
-        // console.log(result.subject_code);
-        // console.log(result.grade);
-      });
-    });
+const dropZone = document.getElementById("dropzone");
+const preview = document.getElementById("preview");
+const fileInput = document.getElementById("file-input");
+const clrbtn = document.getElementById("clear-btn");
+const calbtn = document.getElementById("cal-btn");
+const resultSection = document.getElementById("results");
+const heroSection = document.getElementById("hero");
+const loadingScreen = document.getElementById("loading-screen");
+const sendImgBtn = document.getElementById("send-img");
+const selectRegno = document.getElementById("select-regno");
+const viewGpa = document.getElementById("view-gpa");
+const selectRegScreen = document.getElementById("select-regscreen");
+const errorMessage = document.getElementById("error-message");
+const errorToast = document.getElementById("toast-danger");
+const retryBtn = document.getElementById("retry-btn");
+const closeToastBtn = document.getElementById("close-toast-btn");
+const goBackBtn = document.getElementById("go-back-btn");
+const gotoHome = document.getElementById("go-to-home");
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+
+let globalResponse = [];
+multipleStudentResults = false;
+
+function handleMultipleResults() {
+  data = globalResponse[selectRegno.value];
+  displayResult(data);
+  selectRegScreen.classList.add("hidden");
 }
-testing_response();
+
+function showErrorToast(message) {
+  errorMessage.textContent =
+    message || "An unexcepted error occured. Try again later.";
+  loadingScreen.classList.add("hidden");
+  heroSection.classList.remove("hidden");
+  errorToast.classList.remove("hidden");
+}
+
+function dismissError() {
+  errorToast.classList.add("hidden");
+  fileInput.value = "";
+  preview.classList.add("hidden");
+  document.getElementById("upload-text").classList.remove("hidden");
+}
+
+function displayResult(data) {
+  loadingScreen.classList.add("hidden");
+  resultSection.classList.remove("hidden");
+
+  studentRegno.textContent = data.student_regno;
+  studentName.textContent = data.student_name;
+  resultDisplay.innerHTML += `<li class="w-[90%] bg-white rounded-lg flex px-4 py-2 justify-between gap-4 text-black border-3">
+                <h2>Your GPA is:</h2>
+                <h1 class=" text-4xl font-semibold">
+                  ${data.gpa}
+                </h1>
+          </li>`;
+  data.results.forEach((result) => {
+    resultDisplay.innerHTML += `<li
+              class="w-[90%] bg-blue-700 rounded-lg flex px-4 py-2 justify-between gap-4 border-2 border-white"
+            >
+              <div><h2>${result.subject_code}</h2>
+              <h1 class="font-semibold">
+                ${result.subject_name}
+                </h1>
+              </div>
+              <div class="text-center">
+              <h1 class="text-4xl font-bold">${result.grade}</h1>
+                <h2>Grade</h2>
+              </div>
+            </li>
+            `;
+  });
+}
+async function testing_response() {
+  heroSection.classList.add("hidden");
+  loadingScreen.classList.remove("hidden");
+  const response = await fetch("http://localhost:8000/return/", {
+    method: "GET",
+  });
+  let data = await response.json();
+  console.log(data);
+  if (data.length > 1) {
+    multipleStudentResults = true;
+    console.log("yes");
+    globalResponse = data;
+    data.forEach((stud, index) => {
+      const opt = document.createElement("option");
+      opt.innerHTML = stud.student_regno;
+      opt.value = index;
+      opt.classList.add("bg-white", "text-black", "text-sm");
+      selectRegno.appendChild(opt);
+    });
+    loadingScreen.classList.add("hidden");
+    selectRegScreen.classList.remove("hidden");
+  } else if (data.length == 1) {
+    displayResult(data[0]);
+  } else {
+    alert("error occured. try again.");
+  }
+}
+
+// testing_response();
+dropZone.addEventListener("click", () => fileInput.click());
+dropZone.addEventListener("drop", dropHandler);
+dropZone.addEventListener("dragover", (e) => e.preventDefault());
+window.addEventListener("drop", (e) => {
+  if ([...e.dataTransfer.items].some((item) => item.type === "file")) {
+    e.preventDefault();
+  }
+});
+
+dropZone.addEventListener("dragover", (e) => {
+  const fileItems = [...e.dataTransfer.items].filter(
+    (item) => item.kind === "file"
+  );
+  if (fileItems.length > 0) {
+    e.preventDefault();
+    if (fileItems.some((item) => item.type.startsWith("image/"))) {
+      e.dataTransfer.dropEffect = "copy";
+    } else {
+      e.dataTransfer.dropEffect = "none";
+    }
+  }
+});
+
+window.addEventListener("dragover", (e) => {
+  const fileItems = [...e.dataTransfer.items].filter(
+    (item) => item.kind === "file"
+  );
+  if (fileItems.length > 0) {
+    e.preventDefault();
+    if (!dropZone.contains(e.target)) {
+      e.dataTransfer.dropEffect = "none";
+    }
+  }
+});
+
+function displayImagePreview(files) {
+  if (!files || files.length === 0) {
+    return;
+  }
+  const file = files[0];
+
+  if (!file.type.startsWith("image/")) {
+    showErrorToast("Invalid file type. Upload an image(PNG, JPG, JPEG).");
+    fileInput.value = "";
+    return;
+  }
+  if (file.size > MAX_FILE_SIZE) {
+    const filesize = (file.size / (1024 * 1024)).toFixed(2);
+    showErrorToast(
+      `File too large (${filesize} MB). Maximum allowed size is 5 MB`
+    );
+    fileInput.value = "";
+    return;
+  }
+
+  preview.innerHTML = "";
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  document.getElementById("upload-text").classList.add("hidden");
+  img.src = URL.createObjectURL(file);
+
+  preview.classList.remove("hidden");
+  img.classList.add("object-fill");
+  li.classList.add("w-full", "h-full");
+  img.classList.add("w-full", "h-full", "object-cover", "rounded-lg");
+  li.appendChild(img);
+  preview.appendChild(li);
+
+  calbtn.disabled = false;
+  clrbtn.disabled = false;
+}
+
+function dropHandler(ev) {
+  ev.preventDefault();
+  const fileItems = [...ev.dataTransfer.items]
+    .map((item) => item.getAsFile())
+    .filter((file) => file);
+  displayImagePreview(fileItems);
+}
+
+fileInput.addEventListener("change", (e) => {
+  fileInput.files = e.target.files;
+  displayImagePreview(e.target.files);
+});
+
+clrbtn.addEventListener("click", (ev) => {
+  for (img of preview.querySelectorAll("img")) {
+    URL.revokeObjectURL(img.src);
+  }
+  preview.innerHTML = "";
+  preview.classList.add("hidden");
+  document.getElementById("upload-text").classList.remove("hidden");
+  fileInput.value = "";
+  calbtn.disabled = true;
+  clrbtn.disabled = true;
+});
+
+async function sendImage(e) {
+  e.preventDefault();
+  heroSection.classList.add("hidden");
+  loadingScreen.classList.remove("hidden");
+  const apiUploadenpoint = "http://localhost:8000/calculateGpa/";
+  const formData = new FormData();
+  formData.append("file", fileInput.files[0]);
+  try {
+    const response = await fetch(apiUploadenpoint, {
+      method: "POST",
+      body: formData,
+    });
+    if (!response.ok) throw new error("Upload failed");
+    const data = await response.json();
+    loadingScreen.classList.add("hidden");
+    console.log("Success: ", data);
+    if (data.length > 1) {
+      multipleStudentResults = true;
+      console.log("yes");
+      globalResponse = data;
+      data.forEach((stud, index) => {
+        const opt = document.createElement("option");
+        opt.innerHTML = stud.student_regno;
+        opt.value = index;
+        opt.classList.add("bg-white", "text-black", "text-sm");
+        selectRegno.appendChild(opt);
+      });
+      loadingScreen.classList.add("hidden");
+      selectRegScreen.classList.remove("hidden");
+    } else {
+      displayResult(data);
+    }
+  } catch (error) {
+    if (error instanceof TypeError) {
+      showErrorToast("Couldn't reach the server. Try again later.");
+    }
+  }
+}
+calbtn.addEventListener("click", testing_response);
+sendImgBtn.addEventListener("click", sendImage);
+viewGpa.addEventListener("click", handleMultipleResults);
+
+closeToastBtn.addEventListener("click", dismissError);
+retryBtn.addEventListener("click", () => {
+  errorToast.classList.add("hidden");
+  sendImgBtn.click();
+  ///
+});
+
+goBackBtn.addEventListener("click", () => {
+  resultSection.classList.add("hidden");
+  selectRegScreen.classList.remove("hidden");
+});
+
+gotoHome.addEventListener("click", () => {
+  selectRegScreen.classList.add("hidden");
+  heroSection.classList.remove("hidden");
+});
